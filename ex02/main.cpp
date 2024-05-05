6a1ed7268b17e3941f96ab25b896ae3264b0af86
Bernardo Castelo Branco <bernardo.branco@streambow.com>
2024-05-05 10:34:14 +0100

    ex02 ready to test

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bcastelo <bcastelo@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 22:50:50 by bcastelo          #+#    #+#             */
-/*   Updated: 2024/03/18 22:31:31 by bcastelo         ###   ########.fr       */
+/*   Updated: 2024/05/05 10:28:58 by bcastelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,13 @@ int	main( void )
 	const Animal* i = new Cat();
 	const WrongAnimal* k = new WrongCat();
 	const WrongCat* m = new WrongCat();
-	const Animal* farm[10];
 	//const Animal Test("Dog");
 
 	std::cout << j->getType() << " " << std::endl;
 	std::cout << i->getType() << " " << std::endl;
 	std::cout << k->getType() << " " << std::endl;
 	std::cout << m->getType() << " " << std::endl;
-	i->makeSound(); //will output the cat sound!
+	i->makeSound();
 	j->makeSound();
 	k->makeSound();
 	m->makeSound();
@@ -40,22 +39,5 @@ int	main( void )
 	delete k;
 	delete m;
 
-	Cat ori;
-	std::cout << "Cat cpy = ori" << std::endl;
-	Cat cpy = ori;
-	std::cout << "Cat cpy2( ori )" << std::endl;
-	Cat cpy2( ori );
-	std::cout << "cpy2 = cpy" << std::endl;
-	cpy2 = cpy;
-	std::cout << "Farm:" << std::endl;
-	for (int n = 0; n < 10; n++)
-	{
-		if (n < 5)
-			farm[n] = new Dog();
-		else
-			farm[n] = new Cat();
-	}
-	for (int n = 0; n < 10; n++)
-		delete farm[n];
 	return (0);
 }
